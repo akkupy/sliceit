f53633febaed545149b850eaa8a9980d04923c32
akkupy <akkupy@protonmail.com>
2023-09-06 21:17:33 +0530

    added 404 page

--- web/views.py
+++ web/views.py
@@ -10,4 +10,6 @@ def forwarder(request,code):
     if Link.objects.filter(code=code).exists():
         data = Link.objects.filter(code=code).values()
         return redirect(data[0]['target_url'])
+    else:
+        return render(request,'404.html')
         
