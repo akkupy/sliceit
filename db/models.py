c190582a139e26103165ee87523e72fcb83c3d02
akkupy <akkupy@protonmail.com>
2023-09-08 22:00:26 +0530

    synced with main #4211e9f

--- db/models.py
+++ db/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Link(models.Model):
-    code = models.CharField(max_length=10,primary_key=True)
-    short_url = models.CharField(max_length=20)
-    full_short_url = models.CharField(max_length=30)
+    code = models.CharField(max_length=25,primary_key=True)
+    short_url = models.CharField(max_length=35)
+    full_short_url = models.CharField(max_length=45)
     target_url = models.CharField(max_length=320)
     is_active = models.BooleanField(default=True)
     clicks = models.IntegerField()
