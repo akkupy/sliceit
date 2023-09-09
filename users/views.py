7b96a323cf5a03e57f58773c7121d63ee9cb701b
akkupy <akkupy@protonmail.com>
2023-09-09 11:56:49 +0530

    Added Users

--- users/views.py
+++ users/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def signup(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password1 = request.POST.get('password1')
+        print(email,password)
+
