764cc02a18b7582d99c7bdaad10c60d042b47d87
akkupy <akkupy@protonmail.com>
2023-09-10 09:51:18 +0530

    fixed user registraion and minor bug changes

--- db/models.py
+++ db/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Link(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     code = models.CharField(max_length=25,primary_key=True)
     short_url = models.CharField(max_length=35)
     full_short_url = models.CharField(max_length=45)
